1e84e09b260d6c9fa058dcccd479bca76cf33b02
kalki39 <kalkiram39@gmail.com>
2025-07-11 21:36:36 +0530

    config removed

--- app/src/services/firebase.ts
+++ app/src/services/firebase.ts
@@ -3,20 +3,12 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { firebaseConfig } from '../../../config/firebaseConfig';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyCLmHvfX4fno4OyoncAownr7JS2JlGkZdI",
-  authDomain: "mocktestapp-a63bb.firebaseapp.com",
-  projectId: "mocktestapp-a63bb",
-  storageBucket: "mocktestapp-a63bb.firebasestorage.app",
-  messagingSenderId: "763492275127",
-  appId: "1:763492275127:web:998115e49cb4f4a2c84e32",
-  measurementId: "G-QS2XC11DK6"
-};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
